6719369ccae102153b31d7c3820e23005aef37fb
Harshit Kandhway <kandhway.harshit198@gmail.com>
2024-09-06 10:46:45 +0530

    Initial Setup

---
+++ src/main/java/com/hkgb/talentrack/TalentrackApplication.java
@@ -0,0 +1,13 @@
+package com.hkgb.talentrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalentrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hkgb/talentrack/TalentrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hkgb.talentrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalentrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
